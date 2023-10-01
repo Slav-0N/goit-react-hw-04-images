27786d48e7f9c1fe9c7f764bf1de70be76f58a3a
Slav-0N <SOVILGO@gmail.com>
2023-10-01 22:48:00 +0300

    кнопку лоад мор - переписав з класу на функціональний компонент

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import { Btn } from './Button.styled';
-export class Button extends Component {
-  render() {
-    return (
-      <Btn onClick={this.props.createLoadMore} type="button">
-        <span>Load more</span>
-      </Btn>
-    );
-  }
-}
+export const Button = ({ createLoadMore }) => {
+  return (
+    <Btn onClick={createLoadMore} type="button">
+      <span>Load more</span>
+    </Btn>
+  );
+};
+
+// export class Button extends Component {
+//   render() {
+//     return (
+//       <Btn onClick={this.props.createLoadMore} type="button">
+//         <span>Load more</span>
+//       </Btn>
+//     );
+//   }
+// }
