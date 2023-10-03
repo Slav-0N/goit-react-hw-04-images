916f34686f1e0f9495fe7a5c1dece2ce401e6a35
Slav-0N <SOVILGO@gmail.com>
2023-10-03 18:38:32 +0300

    виправив Modal (handleKeyDown переніс в useEffect)

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,28 +2,17 @@ import { useEffect } from 'react';
 import { Div } from './Modal.styled';
 
 export const Modal = ({ imgAddr, onClose }) => {
-  const handlekeyDown = event => {
-    if (event.code === 'Escape') {
-      onClose();
-    }
-  };
-
   useEffect(() => {
-    console.log('listener did mount');
+    const handlekeyDown = event => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    };
     window.addEventListener('keydown', handlekeyDown);
     return () => {
-      console.log('unmounted');
       window.removeEventListener('keydown', handlekeyDown);
     };
-  }, []);
-
-  // useEffect(() => {
-  //   window.addEventListener('keydown', handlekeyDown);
-  // }, [handlekeyDown]);
-
-  // useEffect(() => {
-  //   return window.removeEventListener('keydown', handlekeyDown);
-  // });
+  }, [onClose]);
 
   const handleBackdropClick = e => {
     if (e.target === e.currentTarget) {
@@ -39,34 +28,3 @@ export const Modal = ({ imgAddr, onClose }) => {
     </Div>
   );
 };
-
-// export default class Modal extends Component {
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.handlekeyDown);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.handlekeyDown);
-//   }
-
-//   handlekeyDown = event => {
-//     if (event.code === 'Escape') {
-//       this.props.onClose();
-//     }
-//   };
-//   handleBackdropClick = e => {
-//     if (e.target === e.currentTarget) {
-//       this.props.onClose();
-//     }
-//   };
-
-//   render() {
-//     return (
-//       <Div>
-//         <div onClick={this.handleBackdropClick}>
-//           <img src={this.props.imgAddr} alt="" target="_self" />
-//         </div>
-//       </Div>
-//     );
-//   }
-// }
